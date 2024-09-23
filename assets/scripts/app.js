ff2c55eb7f3acd25415d505c7778a33bbc048783
Rayman Moses <raymanmoses89@gmail.com>
2024-09-23 15:11:36 +0300

    create cart instances

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -16,11 +16,55 @@ class Product {
     }
 }
 
-console.log(new Product);
+class shoppingCart {
+    items = [];
 
-const productList = {
-     products: [
-          new Product(
+    render(){
+        const cartEl = document.createElement('section');
+        cartEl.innerHTML = `
+         <h2>Total: \$${0}</h2>
+         <button>Order Now</button>
+        `;
+        cartEl.className = 'cart';
+        return cartEl;
+    }
+}
+
+
+class ProductItem{
+    constructor(product){
+        this.product = product;
+    }
+
+    addToCart() {
+        console.log('Add to Cart');
+        console.log(this.product)
+    }
+
+
+    render(){
+        const prodEl = document.createElement('li');
+        prodEl.className= 'product-item'; 
+        prodEl.innerHTML = `
+        <div>
+            <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+            <div class= "product-item__content">
+             <h2>${this.product.title}</h2>
+             <h3>\$${this.product.price}</h3>
+             <p>${this.product.description}</p>
+             <button>Add to Cart</button>
+            </div>
+        </div>
+        `;
+        const addCartButton = prodEl.querySelector('button');
+        addCartButton.addEventListener('click', this.addToCart.bind(this));
+        return prodEl;
+    }
+}
+
+class ProductList {
+    products = [
+        new Product(
             'Pilow',
             image1,
             'A soft pillow!',
@@ -33,32 +77,77 @@ const productList = {
             'The Expensive carpet',
             89.99
         )
-        
-    ],
+    ];
 
+    
     render(){
-        const renderHook = document.getElementById('app');
         const prodList = document.createElement('ul');
         prodList.className = 'product-list';
         for (const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className= 'product-item'; 
-            prodEl.innerHTML = `
-            <div>
-                <img src="${prod.imageUrl}" alt="${prod.title}" >
-                <div class= "product-item__content">
-                 <h2>${prod.title}</h2>
-                 <h3>\$${prod.price}</h3>
-                 <p>${prod.description}</p>
-                 <button>Add to Cart</button>
-                </div>
-            </div>
-            
-            `;
+            const productItem =  new ProductItem(prod);
+            const prodEl = productItem.render();
             prodList.append(prodEl);
         };
-        renderHook.append(prodList);
+        return prodList;
     }
 }
 
-productList.render();
+
+class Shop {
+    render() {
+        const renderHook = document.getElementById('app');
+        const cart = new shoppingCart();
+        const cartEl = cart.render();
+        const productList = new ProductList ();
+        const prodListEl = productList.render();
+
+        renderHook.append(cartEl);
+        renderHook.append(prodListEl)
+    }
+}
+
+const shop = new Shop();
+shop.render();
+// const productList = {
+//      products: [
+//           new Product(
+//             'Pilow',
+//             image1,
+//             'A soft pillow!',
+//             19.99,
+//           ),
+
+//         new Product(
+//             'A Carpet',
+//             image2,
+//             'The Expensive carpet',
+//             89.99
+//         )
+        
+//     ],
+
+//     render(){
+//         const renderHook = document.getElementById('app');
+//         const prodList = document.createElement('ul');
+//         prodList.className = 'product-list';
+//         for (const prod of this.products) {
+//             const prodEl = document.createElement('li');
+//             prodEl.className= 'product-item'; 
+//             prodEl.innerHTML = `
+//             <div>
+//                 <img src="${prod.imageUrl}" alt="${prod.title}" >
+//                 <div class= "product-item__content">
+//                  <h2>${prod.title}</h2>
+//                  <h3>\$${prod.price}</h3>
+//                  <p>${prod.description}</p>
+//                  <button>Add to Cart</button>
+//                 </div>
+//             </div>
+            
+//             `;
+//             prodList.append(prodEl);
+//         };
+//         renderHook.append(prodList);
+//     }
+// }
+
